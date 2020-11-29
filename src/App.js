f53f74e5d529bde39c0d20ec6e995d55aa6b4872
unknown <ziaul935@gmail.com>
2020-11-30 01:42:04 +0600

    routing and nav component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import Home from "./pages/Home"
+import Room from "./pages/Room"
+import SingleRoom from "./pages/SingleRoom"
+import NotFound from "./pages/NotFound"
+import {Route, Switch} from  'react-router-dom'
+import NavBar from "./component/NavBar"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <NavBar/>
+    <Switch>
+    <Route exact path="/" component={Home}/>
+    <Route exact path="/room/" component={Room}/>
+    <Route exact path="/single-room/" component={SingleRoom}/>
+    <Route component={NotFound}/>
+    </Switch>
+    </>
   );
 }
 
---
+++ src/pages/Room.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Room = () => {
+    return (
+        <div>
+            <h1>hellow from</h1>
+        </div>
+    )
+}
+export default Room
