96db476db49761759b4510825a0bb41f00819651
unknown <ziaul935@gmail.com>
2020-12-25 18:31:32 +0600

    roomtype

--- src/component/RoomFilter.jsx
+++ src/component/RoomFilter.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import {useContext} from 'react'
+import {RoomContext} from '../Context'
+import  Title from '../component/Title'
 
-export default function RoomFilter() {
+//get all uqinue values
+
+const getUqinue =(items,value)=>{
+return [...new Set(items.map (item=>item[value]))]
+}
+
+export default function RoomFilter({rooms}) {
+  const context =useContext(RoomContext);
+  //console.log(context);
+  const {handleChange,type,capacity,price,minPrice,maxPrice,minSize,maxSize,breakfast,pets}=context;
+
+  let types =getUqinue(rooms, "type")
+  types =['all',...types];
+  types =types.map((item,index)=>{
+  return<option value={item} key={index}>{item}</option>
+  })
+
     return (
-        <div>
-            <h1>hellow from roomfilert</h1>
+        <section className="filter-container">
+      <Title title="search rooms" />
+      <form className="filter-form">
+                {/*select type  */}
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
         </div>
-    )
+        {/* end select type */}
+            </form>
+        </section>
+    ) 
 }
--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -3,7 +3,7 @@ import Hero from "../component/Hero"
 import Banner from '../component/Banner'
 import {Link} from "react-router-dom"
 import RoomContainer  from '../component/RoomContainer'
-import  Title from '../component/Title' 
+ 
 
 export const Rooms = () => {
     return (
@@ -13,7 +13,7 @@ export const Rooms = () => {
              <Link to="/" className="btn-primary">Return home</Link>
          </Banner>
         </Hero>
-        <Title title="search rooms"/>
+        
         <RoomContainer/>
         </>
     )
